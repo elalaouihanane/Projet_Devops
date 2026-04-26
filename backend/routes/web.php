e754655b8eddedff01a21117105c481e18513065
SAMIRAOUIMING <ouimingsamira@example.com>
2026-04-26 21:51:13 +0100

    SCRUM-73  laravel-change

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/login', function () {
+    return "Login OK";
+})->name('login');
